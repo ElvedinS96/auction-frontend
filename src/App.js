fa30172720e67ac66d89e6d4eabccde23b41c3d8
Lejla Kasum <lkasum1@etf.unsa.ba>
2020-09-27 23:17:40 +0200

    Add helper pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Login from "./pages/Authentication/Login"
 import "./index.css"
 import Footer from "./pages/HeaderFooter/Footer"
 import Header from "./pages/HeaderFooter/Header"
+import PrivacyAndPolicy from "./pages/HelperPages/PrivacyAndPolicy"
+import TermsAndConditions from "./pages/HelperPages/TermsAndConditions"
+import AboutUs from "./pages/HelperPages/AboutUs"
 
 function App() {
 
@@ -15,6 +18,9 @@ function App() {
       <Header />
       <BrowserRouter>
         <Switch>
+          <Route path="/about"><AboutUs /></Route>
+          <Route path="/terms"><TermsAndConditions /></Route>
+          <Route path="/privacy"><PrivacyAndPolicy /></Route>
           <Route path="/login"> <Login baseUrl={BASE_URL} /> </Route>
           <Route path="/register"> <Registration baseUrl={BASE_URL} /> </Route>
         </Switch>
