f23bd1a4773dd0699095afa1cf4f3d32eeb571a1
Lejla Kasum <lkasum1@etf.unsa.ba>
2020-11-12 02:18:42 +0100

    Add ui for setting part of user page

--- src/Components/UserProfile/Settings.jsx
+++ src/Components/UserProfile/Settings.jsx
@@ -4,9 +4,49 @@ import "../../index.css"
 const Settings = props => {
 
     return (
-        <div>
-            Settings
-        </div>
+        <div className="settings-page">
+            <div className="user-box settings">
+                <div className="settings-heading">
+                    <span>Policy and Community</span>
+                </div>
+                <div>
+                    <div className="settings-text" >
+                        Receive updates on bids and seller's offers.
+                        <br />
+                        Stay informed through:
+                        </div>
+                    <div className="settings-text-notf" >
+                        <input type="checkbox" id="notf-email" name="notf-email" value="Email" />
+                        <label for="notf-email">Email</label>
+                    </div>
+                    <div className="settings-text-notf" >
+                        <input type="checkbox" id="notf-push" name="notf-push" value="Push" />
+                        <label for="notf-push">Push Notifications</label>
+                    </div>
+                    <div className="settings-text-notf" >
+                        <input type="checkbox" id="notf-msg" name="notf-msg" value="Message" />
+                        <label for="notf-msg">Text Messages</label>
+                    </div>
+                </div>
+            </div>
+
+            <div className="user-box settings">
+                <div className="settings-heading">
+                    <span>Contact Information</span>
+                </div>
+                <div>
+                    <div className="settings-text" >This information can be edited on your profile.</div>
+                    <div className="settings-text" >
+                        <div>Email&nbsp;</div>
+                        <div className="settings-contact">adam.smith@mail.com</div>
+                    </div>
+                    <div className="settings-text" >
+                        <div>Phone</div>
+                        <div className="settings-contact">555-5555-5555</div>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
