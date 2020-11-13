f18718680387332e2de24a4de72400f02456c262
Lejla Kasum <lkasum1@etf.unsa.ba>
2020-11-13 14:14:39 +0100

    Add ui for user bids on user page

--- src/Components/UserProfile/UserBids.jsx
+++ src/Components/UserProfile/UserBids.jsx
@@ -1,11 +1,63 @@
 import React from "react";
 import "../../index.css"
+import calculateTimeUserBids from "../../Util/calculateTimeUserBids";
 
 const UserBids = props => {
 
+    function UserPrice(userBid, highestBid) {
+        if (userBid < highestBid) {
+            return <td><div className="user-bids-center">$ {userBid.toFixed(2)}</div></td>
+        }
+        return <td><div style={{ color: "#6CC047" }} className="user-bids-center">$ {userBid.toFixed(2)}</div></td>
+    }
+
+    function HighestBid(userBid, highestBid) {
+        if (userBid < highestBid) {
+            return <td><div style={{ color: "#5B9ED6 " }} className="user-bids-center">$ {highestBid.toFixed(2)}</div></td>
+        }
+        return <td><div style={{ color: "#6CC047 " }} className="user-bids-center">$ {highestBid.toFixed(2)}</div></td>
+    }
+
+    function TimeLeft(endTime) {
+        var timeOfAuction = calculateTimeUserBids(Date.now(), endTime)
+        return timeOfAuction
+    }
+
+    const listBids = props.bids.map((bid) =>
+        <tr>
+            <td><img src={bid.imgUrl} /></td>
+            <td colSpan={2}>
+                <div style={{ fontWeight: "bold" }}>{bid.name}</div>
+                <div style={{ color: "#8367D8" }}>#{bid.id}</div>
+            </td>
+            <td>{TimeLeft(bid.auctionEndDate)}</td>
+            {UserPrice(bid.userBid, bid.highestBid)}
+            <td><div className="user-bids-center">{bid.numberOfBids}</div></td>
+            {HighestBid(bid.userBid, bid.highestBid)}
+            <td>
+                <td><div onClick={() => window.location.href = "/product/" + bid.id} className="user-bids-center user-bid-view">VIEW</div></td>
+            </td>
+        </tr>
+    )
+
+
+
     return (
         <div>
-            User bids
+            <div className="user-bids">
+                <table cellspacing="0">
+                    <tr className="user-bids-heading">
+                        <td className="user-bids-item">Item</td>
+                        <td colSpan={2}>Name</td>
+                        <td>Time left</td>
+                        <td><div className="user-bids-center">Your Price</div></td>
+                        <td><div className="user-bids-center">No.Bids</div></td>
+                        <td><div className="user-bids-center">Highest Bid</div></td>
+                        <td><div className="user-bids-center"></div></td>
+                    </tr>
+                    {listBids}
+                </table>
+            </div>
         </div>
     )
 }
--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -6,6 +6,7 @@ import Settings from "../../Components/UserProfile/Settings";
 import UserProfileHeader from "../../Components/UserProfile/UserProfileHeader";
 import "../../index.css"
 import Header from "../HeaderFooter/Header";
+import { useEffect } from "react";
 
 const User = props => {
 
@@ -22,6 +23,51 @@ const User = props => {
         bids: "",
         settings: ""
     })
+
+    const [userBids, setUserBids] = useState([{
+        id: 0,
+        name: "",
+        auctionEndDate: "",
+        userBid: 0,
+        highestBid: 0,
+        numberOfBids: 0,
+        imgUrl: ""
+    }])
+
+
+    useEffect(() => {
+        //TODO Axios api call
+        var bids = [{
+            id: 10,
+            name: "Black Jacket",
+            auctionEndDate: "1605312000000",
+            userBid: 50,
+            highestBid: 100.25,
+            numberOfBids: 3,
+            imgUrl: "https://www.namepros.com/attachments/empty-png.89209/"
+        },
+        {
+            id: 11,
+            name: "Tablet",
+            auctionEndDate: "1605484800000",
+            userBid: 150,
+            highestBid: 500.17,
+            numberOfBids: 1,
+            imgUrl: "https://www.namepros.com/attachments/empty-png.89209/"
+        },
+        {
+            id: 5,
+            name: "Shoes",
+            auctionEndDate: "1606521600000",
+            userBid: 230,
+            highestBid: 230,
+            numberOfBids: 1,
+            imgUrl: "https://www.namepros.com/attachments/empty-png.89209/"
+        }
+        ]
+        setUserBids(bids)
+
+    }, [])
 
     function onHeaderClick(activeButton) {
         switch (activeButton) {
@@ -45,7 +91,7 @@ const User = props => {
             return <Profile />
         }
         else if (profileHeaderActive == "bids") {
-            return <UserBids />
+            return <UserBids bids={userBids} />
         }
         else if (profileHeaderActive == "settings") {
             return <Settings />
